c44cdb38b289c477b912c115c4aea82ee1f6937c
Timur <timos.gooogle@gmail.com>
2021-10-10 16:19:04 +0500

    basic command line interface realization

--- src/ru/itpark/Main.java
+++ src/ru/itpark/Main.java
@@ -1,7 +1,39 @@
 package ru.itpark;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+
+        Store store = new Store();
+
+        while (true) {
+            System.out.println("Введите команду");
+            System.out.println("a - Добавить продукт");
+            System.out.println("l - вывести список продуктов");
+            System.out.println("q - выход");
+
+            String command = scanner.next();
+            switch (command) {
+                case "a":
+                    System.out.println("Введите идентифиатор");
+                    int id = scanner.nextInt();
+                    System.out.println("Введите название");
+                    String name = scanner.next();
+                    System.out.println("Введите стоимость");
+                    int price = scanner.nextInt();
+                    store.add(new Product(id, name, price));
+                    break;
+                case "l":
+                    System.out.println(store.getAll());
+                    break;
+                case "q":
+                    return;
+                default:
+                    System.out.println("Комманда не распознана");
+            }
+        }
     }
 }
